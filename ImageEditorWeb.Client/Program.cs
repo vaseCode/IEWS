ffac656604eba2e062dcd2182ec164c64cac67e8
Antogrado <vas.ex.ll3@gmail.com>
2026-01-16 17:46:08 +0300

    Update files

--- ImageEditorWeb.Client/Program.cs
+++ ImageEditorWeb.Client/Program.cs
@@ -1,4 +1,7 @@
 using ImageEditorWeb.Client;
+using ImageEditorWeb.Client.Services;
+using ImageEditorWeb.Core.Services;
+using ImageEditorWeb.Core.Tools;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,4 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<LayerService>();
+builder.Services.AddScoped<ToolManager>();
+builder.Services.AddScoped<CanvasManager>();
 await builder.Build().RunAsync();
---
+++ ImageEditorWeb.Client/Services/CanvasManager.cs
@@ -0,0 +1,59 @@
+﻿using Blazor.Extensions;
+using Blazor.Extensions.Canvas.Canvas2D;
+using ImageEditorWeb.Shared.Models;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace ImageEditorWeb.Client.Services
+{
+    public class CanvasManager : IAsyncDisposable
+    {
+        private readonly IJSRuntime _jsRuntime;
+        private Canvas2DContext _context;
+        private BECanvasComponent _canvas;
+        public CanvasManager(IJSRuntime jSRuntime)
+        {
+            _jsRuntime =jSRuntime;
+        }
+        public async Task InitializeAsync(string canvasId, BECanvasComponent canvas)
+        {
+            _canvas = canvas;
+            _context = await _canvas.CreateCanvas2DAsync();
+
+            await _context.SetFillStyleAsync("white");
+            await _context.FillRectAsync(0, 0, 800, 600);
+        }
+        public async Task DrawLayer(CanvasLayer layer)
+        {
+            if(!layer.IsVisible||layer.ImageData == null) 
+            { 
+                return; 
+            }  
+            await _context.SaveAsync();
+            await _context.SetGlobalAlphaAsync(layer.Opacity);
+
+
+
+            await _context.RestoreAsync();
+        }
+        public async Task DrawAllLayers(IEnumerable<CanvasLayer> layers)
+        {
+            await ClearCanvas();
+            foreach (var layer in layers.OrderBy(I => I.ZIndex)) 
+            {
+                await DrawLayer(layer);
+            }
+        }
+        private async Task ClearCanvas()
+        {
+            await _context.SetFillStyleAsync("white");
+            await _context.FillRectAsync(0, 0, 800, 600);
+        }
+        public async ValueTask DisposeAsync()
+        {
+            if (_context != null)
+                await _context.DisposeAsync();            
+        }
+    }
+}
