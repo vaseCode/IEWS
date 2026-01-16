461a7134d1bbfed53a59f3130a22cb5073c54e5f
StudentVasiliy <vas.ex.ll3@gmail.com>
2026-01-16 14:46:59 +0300

    Update Enums and Models

---
+++ ImageEditorWeb.Shared/Models/CanvasLayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using System.Drawing;
+
+namespace ImageEditorWeb.Shared.Models
+{
+    public class CanvasLayer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsVisible { get; set; } = true;
+        public float Opacity { get; set; } = 1.0f;
+        public int ZIndex { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+
+    public class EditorState
+    {
+        public List<CanvasLayer> Layers { get; set; } = new();
+        public int ActiveLayerIndex { get; set; }
+        public Size CanvasSize { get; set; }
+        public string CurrentTool { get; set; }
+    }
+
+    public class ToolSettings
+    {
+        public Color ForegroundColor { get; set; } = Color.White;
+        public Color BackgroundColor { get; set; } = Color.Black;
+        public int BrushSize { get; set; }
+        public string BrushType { get; set; } = "Round";
+    }
+}
